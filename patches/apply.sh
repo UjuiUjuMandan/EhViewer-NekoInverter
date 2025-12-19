ba223e9634eb3471e91f54ddd1b6758f840d35d2
ᡠᠵᡠᡳ ᡠᠵᡠ ᠮᠠᠨᡩ᠋ᠠᠨ <125150101+UjuiUjuMandan@users.noreply.github.com>
2025-12-19 17:08:09 +0000

    add 0001-encrypted-client-hello.patch

--- patches/apply.sh
+++ patches/apply.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # $ bash ../patches/apply.sh
 
-git apply ../patches/0001-sni-stripping.patch
+#git apply ../patches/0001-sni-stripping.patch
+git apply ../patches/0001-encrypted-client-hello.patch
 git apply ../patches/0002-repo-rename.patch
 sed -i -e 's/applicationId = "org.moedog.ehviewer"/applicationId = "org.moedog.ehviewer.cc"/' app/build.gradle.kts
